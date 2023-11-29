ee9c0bcdfdbba1a38fd8d94291dd8650b717d1d2
vuvuzelich <louielamani@ukr.net>
2023-11-29 14:34:17 +0200

    Initial Commit

---
+++ firstApp/firstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstAppApp.swift
+//  firstApp
+//
+//  Created by Макс Лахман on 29.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct firstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
